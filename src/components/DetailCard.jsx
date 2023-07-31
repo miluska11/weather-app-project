1151ab3380bfe48ceea0949d6898fd42b9338b47
Miluska Burgos <miluskaburgos44@gmail.com>
2023-07-31 00:02:30 -0500

    components

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -4,13 +4,13 @@ function DetailCard({ data }) {
     <div className="container max-[1003px]:grid-cols-1 justify-center text-center grid gap-5 grid-cols-2 shadow-md mb-auto">
       <div className="my-auto flex  flex-col justify-around bg-color-summarycard h-52">
         <p className=" text-lg">Wind status</p>
-        <p className=" font-bold text-6xl">{Math.round(wind.speed)} mph</p>
+        <p className=" font-bold text-6xl">{Math.round(wind.speed)}mph</p>
         <p className="tracking-wider">WSW</p>
       </div>
       <div className="my-auto flex  flex-col justify-around bg-color-summarycard h-52 ">
         <p className=" text-lg">Humidity</p>
         <p className=" font-bold text-6xl">{main.humidity}%</p>
-        <p className="tracking-wider">WSW</p>
+        <p className="tracking-wider">barra</p>
       </div>
 
       <div className="my-auto  flex  flex-col justify-evenly bg-color-summarycard h-52 ">
